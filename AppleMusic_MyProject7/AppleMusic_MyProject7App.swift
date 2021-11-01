1c7f31bb691f2d641722e31527bdd3e64e5280b4
Sergey Sklyarov <sklyarov.it.020@gmail.com>
2021-11-01 09:28:49 +0100

    Initial Commit

---
+++ AppleMusic_MyProject7/AppleMusic_MyProject7App.swift
@@ -0,0 +1,17 @@
+//
+//  AppleMusic_MyProject7App.swift
+//  AppleMusic_MyProject7
+//
+//  Created by Wizzard Sklyarov on 2021. 11. 01..
+//
+
+import SwiftUI
+
+@main
+struct AppleMusic_MyProject7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleMusic_MyProject7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AppleMusic_MyProject7
+//
+//  Created by Wizzard Sklyarov on 2021. 11. 01..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
